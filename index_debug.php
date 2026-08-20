d9453c4d1960a013cdfa9a4c103c3fcf07687b5c
Shippi und Simion <shqiprim220292@gmail.com>
2026-08-21 01:14:22 +0200

    Keep profile signals and online avatars aligned

--- index_debug.php
+++ index_debug.php
@@ -9,7 +9,7 @@ header('Expires: 0');
 /* Keep a deployable cache marker in tracked code.  The local PHP config is
  * intentionally ignored by git, so a view repair must not depend on a local
  * version bump to reach browsers after a pull request is deployed. */
-$v = sprintf("?v=%s-local-ui108", LUPConfig::$VERSION);
+$v = sprintf("?v=%s-local-ui109", LUPConfig::$VERSION);
 $min = LUPConfig::$MIN;
 $publicBase = 'https://app.www.linkuup.de';
 $shareImage = "{$publicBase}/images/lup-wapp-icon.png";
@@ -101,7 +101,7 @@ $shareImage = "{$publicBase}/images/lup-wapp-icon.png";
   </div>
 
   <script type="text/javascript">
-  window.LUP_BUILD = <?=json_encode(LUPConfig::$VERSION . '-local-ui108')?>;
+  window.LUP_BUILD = <?=json_encode(LUPConfig::$VERSION . '-local-ui109')?>;
   </script>
 
   <script src="node_modules/jquery/dist/jquery.js<?=$v?>"></script>
