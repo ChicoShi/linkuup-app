b57b65d5b750259debd3e1aa2665714e05418afa
Shippi und Simion <shqiprim220292@gmail.com>
2026-08-22 13:02:50 +0200

    Refine ten-step rating dialog and refresh comments immediately

--- js/pages/comments/lup-comments.js
+++ js/pages/comments/lup-comments.js
@@ -95,11 +95,11 @@ angular.module('LUP').config(function($routeProvider) {
 	
 	$scope.savedComment = function() {
 		console.log('CommentsCtrl.savedComment()');
-		ErrorSrvc.showMessage($translate.instant('MSG_THX_VOTE'), $translate.instant('MSGT_THX')).
-			then(function() {
-				CommentSrvc.withOwnComment($scope.data.room).then($scope.loadedOwnComment);
-				$scope.reloadComments();
-			});
+		// Reload immediately. Waiting for the thank-you dialog to be closed made a
+		// freshly written comment appear missing even though it was saved already.
+		CommentSrvc.withOwnComment($scope.data.room).then($scope.loadedOwnComment);
+		$scope.reloadComments();
+		return ErrorSrvc.showMessage($translate.instant('MSG_THX_VOTE'), $translate.instant('MSGT_THX'));
 	};
 	
 	$scope.deleteComment = function(comment) {
